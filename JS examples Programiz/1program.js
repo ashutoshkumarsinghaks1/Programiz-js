c441128d1113976c8236f454b184d96ec0095672
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-08-24 23:09:33 +0530

    Add files via upload

---
+++ JS examples Programiz/1program.js
@@ -0,0 +1,24 @@
+//1. JavaScript Program To Print Hello World
+
+/*We will use these three ways to print 'Hello, World!'.
+
+console.log()
+alert()
+document.write()
+*/
+//1. Using console.log()
+//console.log() is used in debugging the code.
+
+// the hello world program
+console.log("Hello World");
+
+//2. Using alert()
+//The alert() method displays an alert box over the current window with the specified message.
+
+// the hello world program
+alert("Hello, World!");
+
+//3. Using document.write()
+//document.write() is used when you want to print the content to the HTML document.
+// the hello world program
+document.write("Hello, World!");
