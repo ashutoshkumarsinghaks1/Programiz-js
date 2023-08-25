4b48a5f063d284a3120fffaf43efdbcdaad6fd3b
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-08-25 21:32:10 +0530

    Add files via upload

---
+++ JS examples Programiz/2program.js
@@ -0,0 +1,21 @@
+//2.JavaScript Program to Add Two Numbers
+//Example 1: Add Two Numbers
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+//Example 2: Add Two Numbers Entered by the User
+// store input numbers
+const Num1 = parseInt(prompt("Enter the first number "));
+const Num2 = parseInt(prompt("Enter the second number "));
+
+//add two numbers
+const Sum = Num1 + Num2;
+
+// display the sum
+console.log(`The sum of ${Num1} and ${Num2} is ${Sum}`);
