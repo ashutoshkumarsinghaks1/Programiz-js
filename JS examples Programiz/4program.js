1623467d0b0a077b74beed752d9fa72e16703ff2
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-08-28 13:18:03 +0530

    Add files via upload

---
+++ JS examples Programiz/4program.js
@@ -0,0 +1,26 @@
+//Q4.JavaScript Program to Calculate the Area of a Triangle
+//Example 1: Area When Base and Height is Known
+
+const baseValue = prompt("Enter the base of a triangle: ");
+const heightValue = prompt("Enter the height of a triangle: ");
+
+// calculate the area
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(`The area of the triangle is ${areaValue}`);
+
+//Example 2: Area When All Sides are Known
+
+// JavaScript program to find the area of a triangle
+
+const side1 = parseInt(prompt("Enter side1: "));
+const side2 = parseInt(prompt("Enter side2: "));
+const side3 = parseInt(prompt("Enter side3: "));
+
+// calculate the semi-perimeter
+const s = (side1 + side2 + side3) / 2;
+
+//calculate the area
+const areaValue1 = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+
+console.log(`The area of the triangle is ${areaValue1}`);
