f85f4a2c7a9f032e1203d1c2ef1fae4afff83cec
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-05 23:11:31 +0530

    Update 10program.js

--- JS examples Programiz/10program.js
+++ JS examples Programiz/10program.js
@@ -17,3 +17,18 @@ else if (number == 0) {
 else {
      console.log("The number is negative");
 }
+
+
+
+// check if the number is positive, negative or zero
+const number = prompt("Enter a number: ");
+
+if (number >= 0) {
+    if (number == 0) {
+        console.log("The number is zero");
+    } else {
+        console.log("The number is positive");
+    }
+} else {
+    console.log("The number is negative");
+}
