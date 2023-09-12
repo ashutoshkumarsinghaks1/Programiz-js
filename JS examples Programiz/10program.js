99a6885786a39c9aaa8483bab3b4cd6360c77439
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-12 22:36:36 +0530

    Update 10program.js

--- JS examples Programiz/10program.js
+++ JS examples Programiz/10program.js
@@ -63,3 +63,16 @@ if(number % 2 == 0) {
 else {
     console.log("The number is odd.");
 }
+
+
+
+
+
+// program to convert date to number
+// create date
+const d1 = new Date();
+console.log(d1);
+
+// converting to number
+const result = d1.getTime();
+console.log(result);
