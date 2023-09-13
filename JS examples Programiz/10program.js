dc7143550767ae217607677432e3785b03e20df8
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-13 22:19:08 +0530

    Update 10program.js

--- JS examples Programiz/10program.js
+++ JS examples Programiz/10program.js
@@ -76,3 +76,35 @@ console.log(d1);
 // converting to number
 const result = d1.getTime();
 console.log(result);
+
+
+
+
+// program to write to console
+
+// passing number 
+console.log(8);
+
+// passing string
+console.log('hello');
+
+// passing variable
+const x = 'hello';
+console.log(x);
+
+// passing function
+function sayName() {
+    return 'Hello John';
+}
+console.log(sayName());
+
+// passing string and a variable
+const name = 'John';
+console.log('Hello ' + name);
+
+// passing object
+let obj = {
+    name: 'John',
+    age: 28
+}
+console.log(obj);
