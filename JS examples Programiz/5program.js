f325a42059a56197333d3fb5aada65dc2073a776
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-15 19:16:51 +0530

    Update 5program.js

--- JS examples Programiz/5program.js
+++ JS examples Programiz/5program.js
@@ -16,3 +16,17 @@ b = temp;
 
 console.log(`The value of a after swapping: ${a}`);
 console.log(`The value of b after swapping: ${b}`);
+
+
+
+// program to encode a string to Base64
+// defining the string
+const str = "Learning JavaScript"; 
+
+// encoding the string
+const result = window.btoa(str);
+console.log(result);
+
+// decoding the string
+const result1 = window.atob(result);
+console.log(result1);
