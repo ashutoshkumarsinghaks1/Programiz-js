9e8d02c3ce05677102b4facfc777ecd991d36b9a
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-17 23:20:39 +0530

    Update 7program.js

--- JS examples Programiz/7program.js
+++ JS examples Programiz/7program.js
@@ -10,3 +10,20 @@ const factor = 0.621371;
 const miles = kilometers * factor;
 
 console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
+
+
+
+// program to display the sum of natural numbers
+
+// take input from the user
+const number = parseInt(prompt('Enter a positive integer: '));
+
+let sum = 0;
+
+// looping from i = 1 to number
+// in each iteration, i is increased by 1
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+
+console.log('The sum of natural numbers:', sum);
