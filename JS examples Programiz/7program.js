47ea3a0ff5563f7042a72a2caca122aa51a29d11
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-18 20:40:07 +0530

    Update 7program.js

--- JS examples Programiz/7program.js
+++ JS examples Programiz/7program.js
@@ -27,3 +27,23 @@ for (let i = 1; i <= number; i++) {
 }
 
 console.log('The sum of natural numbers:', sum);
+
+
+
+// program to find the LCM of two integers
+
+// take input
+const num1 = prompt('Enter a first positive integer: ');
+const num2 = prompt('Enter a second positive integer: ');
+
+// higher number among number1 and number2 is stored in min
+let min = (num1 > num2) ? num1 : num2;
+
+// while loop
+while (true) {
+    if (min % num1 == 0 && min % num2 == 0) {
+        console.log(`The LCM of ${num1} and ${num2} is ${min}`);
+        break;
+    }
+    min++;
+}
