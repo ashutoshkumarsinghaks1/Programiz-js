fbfab7da32145ba21be36f81298c2cceaef53c7d
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-23 20:22:55 +0530

    Update 8programs.js

--- JS examples Programiz/8programs.js
+++ JS examples Programiz/8programs.js
@@ -10,3 +10,13 @@ const fahrenheit = celsius * 1.8 + 32;
 console.log(
   `${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`
 );
+
+
+
+// program to display text 5 times
+const n = 5;
+
+// looping from i = 1 to 5
+for (let i = 1; i <= n; i++) {
+    console.log(`I love JavaScript.`);
+}
