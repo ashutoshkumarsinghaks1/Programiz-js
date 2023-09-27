80e77b897c2c6e8b6d1811f1eb86bf8f5a8df2bf
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-09-27 23:55:54 +0530

    Update 4program.js

--- JS examples Programiz/4program.js
+++ JS examples Programiz/4program.js
@@ -24,3 +24,50 @@ const s = (side1 + side2 + side3) / 2;
 const areaValue1 = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
 
 console.log(`The area of the triangle is ${areaValue1}`);
+
+
+// program to shuffle the deck of cards
+
+// declare card elements
+const suits = ["Spades", "Diamonds", "Club", "Heart"];
+const values = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+];
+
+// empty array to contain cards
+let deck = [];
+
+// create a deck of cards
+for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+        let card = { Value: values[x], Suit: suits[i] };
+        deck.push(card);
+    }
+}
+
+// shuffle the cards
+for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+}
+
+console.log('The first five cards are:');
+
+// display 5 results
+for (let i = 0; i < 5; i++) {
+    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
+}
