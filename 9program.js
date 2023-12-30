ab8469e43419a58f23cb75e332f222361f269b52
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-12-30 21:57:35 +0530

    Update 9program.js

--- 9program.js
+++ 9program.js
@@ -2,3 +2,13 @@
 // generating  a random number
 const a = Math.random();
 console.log(a);
+
+// program to replace a character of a string
+
+const string = 'Mr Red has a red house and a red car';
+
+// replace the characters
+const newText = string.replace('red', 'blue');
+
+// display the result
+console.log(newText);
